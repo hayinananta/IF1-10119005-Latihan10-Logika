d76fe3dc352eb053f04748a446f2ab7d4a463957
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 11:16:14 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan10/PBO110119005Latihan10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan10;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Logika
+ */
+
+public class PBO110119005Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
